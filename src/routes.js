a299416650286ca2fedde9ffe56225b0d0f35b8a
fillipeags <fillipe484@gmail.com>
2021-03-21 19:37:52 -0300

    UserController Crud

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,9 @@ const UserController = require('./app/controllers/UserController');
 const router = Router();
 
 router.get('/users', UserController.index);
+router.get('/users/:id', UserController.show);
+router.delete('/users/:id', UserController.delete);
+router.post('/users', UserController.store);
+router.put('/users/:id', UserController.update);
 
 module.exports = router;
