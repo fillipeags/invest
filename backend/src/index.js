a07569c8b8e92bc1c8f05240dfc4dac49f25f44a
Fillipe Augusto <fillipe484@gmail.com>
2021-03-28 21:06:14 -0300

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 // eslint-disable-next-line no-console
-app.listen(3000, () => console.log('🔥 Server started running at http://localhost:3000'));
+app.listen(3333, () => console.log('🔥 Server started running at http://localhost:3333'));
