fbe248006f06c65ec774934e3c09e80dbe281fdc
Fillipe Augusto <fillipe484@gmail.com>
2021-03-30 09:40:46 -0300

    fix app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Brokers from './components/Brokers';
 import Companies from './components/Companies';
 import Transactions from './components/Transactions';
 
+
 export function App() {
   return (
 		<>
